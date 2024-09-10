a6d0f96b56c8999d033afdce54712eaa8aaa66d3
roxeannechico <roseannechico22@gmail.com>
2024-09-10 12:15:07 +0800

    initial commit

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 const firstFavNum = 22;
 const secondFavNum = 9;
 
-//Addition
+console.log('Addition')
 const sum = firstFavNum + secondFavNum;
 console.log(sum);
-//Subtraction
+
+console.log('Subtraction')
 const difference = firstFavNum - secondFavNum;
 console.log(difference);
-//Multiplication
+
+console.log('Multiplication')
 const product = firstFavNum * secondFavNum;
 console.log(product);
-//Division
+
+console.log('Division')
 const quotient = firstFavNum / secondFavNum;
 console.log(quotient);
-//Modulus
+
+console.log('Modulus')
 const remainder = firstFavNum % secondFavNum;
 console.log(remainder);
-//Exponent (Power)
+
+console.log('Exponent (Power)')
 const power = firstFavNum ** secondFavNum;
 console.log(remainder);
